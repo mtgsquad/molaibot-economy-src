07fcc8312ee9ed6a7f0def4738d74bd926569294
mahirmolai27 <69717367+mahirmolai27@users.noreply.github.com>
2021-03-27 15:40:46 +0500

    Init

---
+++ balance.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'balance',
+    description: 'Check your account balance!',
+    usage: '',
+    aliases: ['bal'],
+    run: (client, message, args, profileData) => {
+        let balEmbed = new Discord.MessageEmbed()
+        .setAuthor(`${message.author.tag}`)
+        .setTitle(`${message.author.tag}'s Balance`)
+        .setDescription(`Wallet: ${profileData.mCoins} mCoins\nBank: ${profileData.bank} mCoins`)
+        .setColor('RANDOM')
+        .setTimestamp()
+        .setFooter('MolaiBOT - Made By MTGSquad')
+
+        message.channel.send(balEmbed)
+    }
+}
---
+++ reset.js
@@ -0,0 +1,10 @@
+const profileSchema = require('../../models/profileSchema');
+
+module.exports = {
+    name: 'reset',
+    description: 'Reset the amount of mCoins a user has!',
+    usage: '<@user>',
+    run: async(client, message, args, profileData) => {
+        
+    },
+}
